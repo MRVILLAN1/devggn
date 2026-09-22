5bed2bd8f4ae3b4d35c11211cb5f8044cf8ec36e
MRVILLAN1 <157270721+MRVILLAN1@users.noreply.github.com>
2026-09-22 12:03:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ YTUB_COOKIES = """
 
 API_ID = int(getenv("API_ID", "25520653"))
 API_HASH = getenv("API_HASH", "5e99595580628982d3fdb6066657b8ca")
-BOT_TOKEN = getenv("BOT_TOKEN", "8980179743:AAFjRKVj564-SQZC33FFIRwFKb_3E7FmlHc")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "5825448865").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://toonworld7493_db_user:1hmtRk88JEwypeqp@cluster0.q1sljkw.mongodb.net/?appName=Cluster0")
 LOG_GROUP = getenv("LOG_GROUP", "-1003746021917")
